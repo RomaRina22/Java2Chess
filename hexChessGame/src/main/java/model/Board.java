a5faa87ddb5eab253664603da5322f72ce98aef9
RomaRina22 <s22romarina@venta.lv>
2023-01-30 04:30:16 +0200

    Board constructor, Spot functions

--- hexChessGame/src/main/java/model/Board.java
+++ hexChessGame/src/main/java/model/Board.java
@@ -6,4 +6,8 @@ public class Board {
 	private Spot[][] spots;
 	private int[] playerdirections = {1,3,0,0,0,0};
 	
+	public Board (int size) {
+		this.spots = new Spot[size][size];
+	}
+	
 }
--- hexChessGame/src/main/java/model/Spot.java
+++ hexChessGame/src/main/java/model/Spot.java
@@ -2,5 +2,31 @@ package model;
 
 public class Spot {
 	private Piece piece;
-	private SpotStates state;
+	private SpotStates state = SpotStates.CALM;
+	
+	public Spot() {
+		
+	}
+
+	public Piece getPiece() {
+		return piece;
+	}
+
+	public void setPiece(Piece piece) {
+		this.piece = piece;
+	}
+	
+	public Piece removePiece() {
+		Piece temp = this.piece;
+		this.piece = null;
+		return temp;
+	}
+	public SpotStates getState() {
+		return state;
+	}
+
+	public void setState(SpotStates state) {
+		this.state = state;
+	}
+	
 }
