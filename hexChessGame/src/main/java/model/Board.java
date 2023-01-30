6ca6882aa1333ab42e721c3226689a19e38bde1d
RomaRina22 <s22romarina@venta.lv>
2023-01-30 04:46:32 +0200

    Added board name string

--- hexChessGame/src/main/java/model/Board.java
+++ hexChessGame/src/main/java/model/Board.java
@@ -3,10 +3,12 @@ package model;
 import java.util.ArrayList;
 
 public class Board {
+	private String name;
 	private Spot[][] spots;
 	private int[] playerdirections = {1,3,0,0,0,0};
 	
-	public Board (int size) {
+	public Board (int size, String name) {
+		this.name = name;
 		this.spots = new Spot[size][size];
 	}
 	
