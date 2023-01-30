a5f6c429ef1c0ec63961f1d79958c45e234840ce
RomaRina22 <s22romarina@venta.lv>
2023-01-30 04:20:54 +0200

    Added more variables to models

--- hexChessGame/src/main/java/model/Board.java
+++ hexChessGame/src/main/java/model/Board.java
@@ -4,5 +4,6 @@ import java.util.ArrayList;
 
 public class Board {
 	private Spot[][] spots;
+	private int[] playerdirections = {1,3,0,0,0,0};
 	
 }
--- hexChessGame/src/main/java/model/SpotStates.java
+++ hexChessGame/src/main/java/model/SpotStates.java
@@ -1,5 +1,5 @@
 package model;
 
 public enum SpotStates {
-CALM, SELECTED, AVAILABLE, TARGETTED
+CALM, SELECTED, AVAILABLE, TARGETTED, LOCKED
 }
