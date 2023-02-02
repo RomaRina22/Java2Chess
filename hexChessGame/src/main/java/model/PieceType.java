8389e605a9d81927ed2f4d404bb90ca12938160f
RomaRina22 <s22romarina@venta.lv>
2023-02-02 18:57:11 +0200

    PieceType getters, setters, constructor

--- hexChessGame/src/main/java/model/PieceType.java
+++ hexChessGame/src/main/java/model/PieceType.java
@@ -7,4 +7,30 @@ public class PieceType {
 	private String name;
 	private URL image;
 	private ArrayList<MovePattern> moves;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public URL getImage() {
+		return image;
+	}
+	public void setImage(URL image) {
+		this.image = image;
+	}
+	public ArrayList<MovePattern> getMoves() {
+		return moves;
+	}
+	public void setMoves(ArrayList<MovePattern> moves) {
+		this.moves = moves;
+	}
+	public PieceType(String name, URL image, ArrayList<MovePattern> moves) {
+		super();
+		setName(name);
+		setImage(image);
+		setMoves(moves);
+	}
+	
+	
 }
