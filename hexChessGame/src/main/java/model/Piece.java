d48a8340093b7d608d0c04f9a673392e1681783b
RomaRina22 <s22romarina@venta.lv>
2023-02-02 18:38:28 +0200

    Piece getters, setters, constructor

--- hexChessGame/src/main/java/model/Piece.java
+++ hexChessGame/src/main/java/model/Piece.java
@@ -3,4 +3,21 @@ package model;
 public class Piece {
 	private Player owner;
 	private PieceType type;
+	public Player getOwner() {
+		return owner;
+	}
+	public void setOwner(Player owner) {
+		this.owner = owner;
+	}
+	public PieceType getType() {
+		return type;
+	}
+	public void setType(PieceType type) {
+		this.type = type;
+	}
+	public Piece(Player owner, PieceType type) {
+		super();
+		this.owner = owner;
+		this.type = type;
+	}
 }
