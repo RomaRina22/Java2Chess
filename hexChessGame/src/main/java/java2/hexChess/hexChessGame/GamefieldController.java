bb9981cf78b598906523262042e5fa9d46ebaf6a
RomaRina22 <s22romarina@venta.lv>
2023-02-03 00:58:31 +0200

    gamefield initialize

--- hexChessGame/src/main/java/java2/hexChess/hexChessGame/GamefieldController.java
+++ hexChessGame/src/main/java/java2/hexChess/hexChessGame/GamefieldController.java
@@ -1,6 +1,14 @@
 package java2.hexChess.hexChessGame;
+import java.net.URL;
+
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class GamefieldController {
-
+	@FXML AnchorPane anchor;
+	public void initialize() {
+		
+	}
 }
