5c1e7d99c319aa1edf4cc9700d4b66d86dc59786
RomaRina22 <s22romarina@venta.lv>
2023-02-02 03:07:37 +0200

    added giving coords to spots on board generation

--- hexChessGame/src/main/java/model/Board.java
+++ hexChessGame/src/main/java/model/Board.java
@@ -10,6 +10,11 @@ public class Board {
 	public Board (int size, String name) {
 		this.name = name;
 		this.spots = new Spot[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				spots[i][j] = new Spot(i, j);
+			}
+		}
 	}
 	
 }
