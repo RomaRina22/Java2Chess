9aaf3253cd03f42d2243e7df31d508ddd73a4b1e
RomaRina22 <s22romarina@venta.lv>
2023-02-02 18:54:12 +0200

    GameSettings getters, setters

--- hexChessGame/src/main/java/model/GameSettings.java
+++ hexChessGame/src/main/java/model/GameSettings.java
@@ -8,4 +8,30 @@ public class GameSettings {
 	private GameState gameState;
 	private ArrayList<Player> players;
 	private int gameTurn;
+	public Board getActiveBoard() {
+		return ActiveBoard;
+	}
+	public void setActiveBoard(Board activeBoard) {
+		ActiveBoard = activeBoard;
+	}
+	public GameState getGameState() {
+		return gameState;
+	}
+	public void setGameState(GameState gameState) {
+		this.gameState = gameState;
+	}
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
+	}
+	public int getGameTurn() {
+		return gameTurn;
+	}
+	public void setGameTurn(int gameTurn) {
+		this.gameTurn = gameTurn;
+	}
+	
+	
 }
