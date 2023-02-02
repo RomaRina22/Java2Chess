e687cfb1b99ac4e6248c9f209f23ccedd3642760
RomaRina22 <s22romarina@venta.lv>
2023-02-02 03:40:11 +0200

    added player contstructor, getters, setters

--- hexChessGame/src/main/java/model/Player.java
+++ hexChessGame/src/main/java/model/Player.java
@@ -3,7 +3,31 @@ package model;
 import javafx.scene.paint.Color;
 
 public class Player {
+	static private int playercounter = 0;
 	private String name;
 	private int id;
 	private Color color;
+	
+	public Player (String name, Color color) {
+		setName(name);
+		id = playercounter++;
+		setColor(color);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	
 }
