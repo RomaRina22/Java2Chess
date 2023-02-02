6352242831372c0e865fdd93f72778af73be8644
RomaRina22 <s22romarina@venta.lv>
2023-02-02 19:37:20 +0200

    Board functions
    getters, setters, updated constructor, get spot by coord, get available moves, moves from spot

--- hexChessGame/src/main/java/model/Board.java
+++ hexChessGame/src/main/java/model/Board.java
@@ -1,15 +1,18 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Board {
 	private String name;
 	private Spot[][] spots;
-	private int[] playerdirections = {1,4,0,0,0,0}; //default player movement directions 
-	private int currentPlayerTurn = 0; 
+	private int[] playerDirections = {1,4,0,0,0,0}; //default player movement directions 
+	private int currentPlayerTurn = 0;
+	private int size;
 	
 	public Board (int size, String name) {
 		this.name = name;
+		this.size = size;
 		this.spots = new Spot[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
@@ -18,4 +21,74 @@ public class Board {
 		}
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCurrentPlayerTurn() {
+		return currentPlayerTurn;
+	}
+
+	public void setCurrentPlayerTurn(int currentPlayerTurn) {
+		this.currentPlayerTurn = currentPlayerTurn;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setPlayerDirections(int[] playerDirections) {
+		this.playerDirections = playerDirections;
+	}
+
+	private Spot getSpotByCoords (int[] coords) {
+		if (coords[0] > size || coords[1] > size) {
+			return null;
+		}
+		else {
+			return spots[coords[0]][coords[1]];
+		}
+	}
+	
+	public HashSet<Spot> getAvailableMoves (Spot origin) {
+		Player owner = origin.getPiece().getOwner();
+		int ownerDirection = playerDirections[GameSettings.allPlayers.indexOf(owner)];
+		PieceType piece = origin.getPiece().getType();
+		ArrayList<MovePattern> moves = piece.getMoves();
+		HashSet<Spot> returnSpots = new HashSet<>();
+		returnSpots = movesFrom (origin, moves, ownerDirection);
+		return returnSpots;		
+	}
+	
+	private HashSet<Spot> movesFrom (Spot origin, ArrayList<MovePattern> moves, int ownerDirection) {
+		HashSet<Spot> returnSpots = new HashSet<>();
+		if (moves == null) {
+			returnSpots.add(origin);
+		}
+		else {
+			Spot turtle;
+			for (MovePattern currentMove: moves) {
+				turtle = origin;
+				int movingDirection = currentMove.getDirection() + ownerDirection-1;
+				int i = 0;
+				while (i <= currentMove.getMaxDistance()) {
+					turtle = getSpotByCoords(turtle.getNeighborCoords(movingDirection)); //move turtle forward by one space
+					if (turtle == null) {break;} //if going off board stop moving
+					if (turtle.getPiece() != null && !currentMove.getPassthrough()) { //if current spot is occupied and piece cant pass add the spot to moves and go to next move
+						returnSpots.add(turtle);
+						break;
+					}
+					if (i >= currentMove.getMinDistance()) { //if within movement area do next move
+						returnSpots.addAll(movesFrom(turtle, currentMove.getNextMove(), ownerDirection));
+					}
+					i++;
+				}
+			}
+		}
+		return returnSpots;
+	}
 }
