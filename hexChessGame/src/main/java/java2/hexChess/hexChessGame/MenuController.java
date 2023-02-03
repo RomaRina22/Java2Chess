321e035e71e89bf75d109d6141de46d0df30603d
RomaRina22 <s22romarina@venta.lv>
2023-02-03 04:41:31 +0200

    Menu onActions

--- hexChessGame/src/main/java/java2/hexChess/hexChessGame/MenuController.java
+++ hexChessGame/src/main/java/java2/hexChess/hexChessGame/MenuController.java
@@ -4,5 +4,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
 public class MenuController {
-
+	public void playGame() {
+		App.mainStage.setScene(App.Scenes.get("gamefield"));
+	}
+	public void openOptions() {
+		App.mainStage.setScene(App.Scenes.get("options"));
+	}
+	public void quitGame() {
+		App.mainStage.close();
+	}
 }
